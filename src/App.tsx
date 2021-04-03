2a6b7960a94ebad6451a356a99fbeabce12abe35
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-04-03 23:21:11 +0200

    Add switch to toggle live feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 function App() {
-  const { data, error } = useSWR("https://www.reddit.com/r/GlobalOffensive.json", fetcher);
+  const [refreshData, setRefreshData] = useState(false);
+  const { data, error } = useSWR("https://www.reddit.com/r/GlobalOffensive.json", fetcher, {
+    refreshInterval: refreshData ? 10000 : undefined,
+  });
   console.log(data);
   return (
     <div>
+      <Toggle id="live-feed" label="Live feed" checked={refreshData} onToggle={setRefreshData} />
       <div>
         {data?.data?.children?.map((post: any) => (
-          <div key={post.data.permalink}>
-            <h3>{post.data.title}</h3>
-          </div>
+          <>
+            <Post key={post.data.permalink} title={post.data.title} postedBy="jesse" />
+          </>
         ))}
       </div>
     </div>
   );
 }
+
+interface ToggleProps {
+  id: string;
+  label: string;
+  checked: boolean;
+  onToggle: (newValue: boolean) => void;
+}
+
+const Toggle: FC<ToggleProps> = ({ id, label, checked, onToggle }) => {
+  const toggleClasses = checked
+    ? "absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out bg-blue-400 transform translate-x-full"
+    : "absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out";
+
+  return (
+    <label htmlFor={id} className="mt-3 inline-flex items-center cursor-pointer">
+      <span className="relative">
+        <span className="block w-10 h-6 bg-gray-300 rounded-full shadow-inner"></span>
+        <span className={toggleClasses}>
+          <input id={id} type="checkbox" className="absolute opacity-0 w-0 h-0" onClick={() => onToggle(!checked)} />
+        </span>
+      </span>
+      <span className="ml-3 text-sm">{label}</span>
+    </label>
+  );
+};
+
+interface PostProps {
+  title: string;
+  postedBy: string;
+}
+
+const Post: FC<PostProps> = ({ title, postedBy }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{postedBy}</p>
+    </div>
+  );
+};
 
 export default App;
