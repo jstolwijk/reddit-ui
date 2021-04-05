1b8fa5351b462efa4cdf119d6e12d6b7bf747f5c
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-04-05 22:28:58 +0200

    Lower padding on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -355,7 +355,7 @@ const Post: FC<PostProps> = ({
   const bgColor = stickied ? "bg-yellow-100" : "bg-white";
 
   return (
-    <div className={`p-2 m-4 rounded ${bgColor} shadow-lg divide-y font-extralight`}>
+    <div className={`p-2 lg:mx-4 my-2 lg:rounded-lg ${bgColor} shadow-lg divide-y font-extralight`}>
       <div className="p-1">
         <TitleLink externalUrl={url} internalUrl={"/r/" + subReddit + "/comments/" + id}>
           <h2 className="text-xl font-semibold">{title}</h2>
