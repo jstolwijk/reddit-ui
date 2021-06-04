96980e7e593f3d7cf411cc7d073ba79f3b43a682
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-06-04 21:35:37 +0200

    Improve post loading

--- src/components/subreddit/index.tsx
+++ src/components/subreddit/index.tsx
@@ -236,7 +236,7 @@ export default function SubReddit() {
           </Stack>
           <Stack>
             {error && <SubredditError statusCode={error.status} />}
-            {!error && !data && <p>Pending</p>}
+            {!error && !data && <LoadingPosts />}
             {!error &&
               data
                 ?.flatMap((d) => d?.data?.children)
@@ -282,6 +282,96 @@ export default function SubReddit() {
   );
 }
 
+const LoadingPosts = () => {
+  return (
+    <>
+      <Block>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+            <div className="space-y-2">
+              <div className="h-4 bg-gray-300 rounded"></div>
+              <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+            </div>
+          </div>
+        </div>
+      </Block>
+      <Block>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+            <div className="space-y-2">
+              <div className="h-4 bg-gray-300 rounded"></div>
+              <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+            </div>
+          </div>
+        </div>
+      </Block>
+      <Block>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+            <div className="space-y-2">
+              <div className="h-4 bg-gray-300 rounded"></div>
+              <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+            </div>
+          </div>
+        </div>
+      </Block>
+      <Block>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+            <div className="space-y-2">
+              <div className="h-4 bg-gray-300 rounded"></div>
+              <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+            </div>
+          </div>
+        </div>
+      </Block>
+      <Block>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+            <div className="space-y-2">
+              <div className="h-4 bg-gray-300 rounded"></div>
+              <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+            </div>
+          </div>
+        </div>
+      </Block>
+      <Block>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+            <div className="space-y-2">
+              <div className="h-4 bg-gray-300 rounded"></div>
+              <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+            </div>
+          </div>
+        </div>
+      </Block>
+    </>
+  );
+};
 interface SubredditErrorProps {
   statusCode: number;
 }
