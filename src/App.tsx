05d481cc75ef02a9f91235d03572439a8c11595d
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-06-06 21:00:21 +0200

    Fix cols for pending comments

--- src/App.tsx
+++ src/App.tsx
@@ -210,33 +210,35 @@ const Comment: FC<any> = ({ comment, depth }) => {
 
 const PendingCommentsPage = () => {
   return (
-    <Container>
-      <Stack>
-        <Block>
-          <div className="animate-pulse flex space-x-4">
-            <div className="flex-1 space-y-4 py-1">
-              <div className="h-8 bg-gray-300 rounded w-3/4"></div>
-              <div className="space-y-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-                <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-                <div className="h-4 bg-gray-300 rounded w-3/6"></div>
-                <div className="h-4 bg-gray-300 rounded w-4/6"></div>
-                <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+    <div className="col-span-10">
+      <Container>
+        <Stack>
+          <Block>
+            <div className="animate-pulse flex space-x-4">
+              <div className="flex-1 space-y-4 py-1">
+                <div className="h-8 bg-gray-300 rounded w-3/4"></div>
+                <div className="space-y-2">
+                  <div className="h-4 bg-gray-300 rounded"></div>
+                  <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+                  <div className="h-4 bg-gray-300 rounded w-3/6"></div>
+                  <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+                  <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+                </div>
               </div>
             </div>
-          </div>
-        </Block>
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-        <PendingComment />
-      </Stack>
-    </Container>
+          </Block>
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+          <PendingComment />
+        </Stack>
+      </Container>
+    </div>
   );
 };
 
